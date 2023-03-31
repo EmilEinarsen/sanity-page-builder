c09d4e61193108c81aebbe53ff280a04b90c9f02
EmilEinarsen <emil@hagatun.se>
2023-03-31 10:40:48 -0400

    feat(hero.js): :sparkles: first schema for testing

---
+++ schemas/hero.js
@@ -0,0 +1,78 @@
+export const hero = defineType({
+  type: 'object',
+  name: 'hero',
+  title: 'Hero',
+  icon: Star,
+  fields: [
+		{
+			name: 'title',
+			title: 'Title',
+			type: 'string'
+		},
+		{
+			name: 'subtitle',
+			title: 'Subtitle',
+			type: 'string',
+		},
+    {
+      title: 'Background Type',
+      name: 'bgType',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Photo', value: 'photo' },
+          { title: 'Video', value: 'video' }
+        ],
+        layout: 'radio',
+        direction: 'horizontal'
+      },
+      validation: Rule => Rule.required()
+    },
+    {
+      ...customImage({ name: 'image', title: 'Background Image' }),
+      hidden: ({ parent }) => {
+        return parent.bgType !== 'photo'
+      }
+    },
+    {
+			name: 'videoFile',
+			title: 'Background Video',
+			type: 'file',
+			options: {
+				accept: 'video/*'
+			},
+      hidden: ({ parent }) => {
+        return parent.bgType !== 'video'
+      }
+    },
+		{
+			type: 'string',
+			name: 'theme',
+			title: 'Theme',
+			description: 'Background image theme; Text color will be highest contrast',
+			options: {
+				list: [
+					{title: 'Light', value: 'light'},
+					{title: 'Dark', value: 'dark'}
+				],
+				layout: 'radio'
+			},
+      hidden: ({ parent }) => {
+        return parent.bgType !== 'photo'
+      }
+		},
+  ],
+  preview: {
+    select: {
+      photo: 'photo',
+      content: 'content.0.children'
+    },
+    prepare({ photo, content }) {
+      return {
+        title: 'Start Page Hero',
+        subtitle: content && content[0]?.text,
+        media: photo
+      }
+    }
+  }
+})
